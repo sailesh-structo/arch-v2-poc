62aff6f0f3cb96bdb700cc17491ee5349de22e8e
Sailesh <sailesh.kumar@structo3d.com>
2023-10-29 16:30:14 +0800

    Added remote actions service

---
+++ edge/remote-actions/main.py
@@ -0,0 +1,52 @@
+import os
+from confluent_kafka import Consumer, Producer
+
+# Configure the source and target Kafka brokers
+source_broker = os.environ.get('SOURCE_BROKER')  # Replace with the address of the source broker
+target_broker = os.environ.get('TARGET_BROKER')  # Replace with the address of the target broker
+source_topic_name = os.environ.get('SOURCE_TOPIC')  # Replace with your Kafka topic name
+target_topic_name = os.environ.get('TARGET_TOPIC')  # Replace with your Kafka topic name
+
+# Configure the consumer
+consumer_config = {
+    'bootstrap.servers': source_broker,
+    'group.id': 'edge-consumer-' + os.environ.get('MACHINE_ID'),
+    'auto.offset.reset': 'earliest'
+}
+
+# Configure the producer
+producer_config = {
+    'bootstrap.servers': target_broker
+}
+
+# Create a Kafka consumer and subscribe to the topic
+consumer = Consumer(consumer_config)
+consumer.subscribe([source_topic_name])
+
+# Create a Kafka producer
+producer = Producer(producer_config)
+
+# Continuously consume and produce messages
+try:
+    while True:
+        msg = consumer.poll(1.0)
+        if msg is None:
+            continue
+        if msg.error():
+            if msg.error().code() == Consumer.ERR__PARTITION_EOF:
+                continue
+            else:
+                print(f"Error while consuming message: {msg.error()}")
+                break
+
+        # Produce the received message to the target broker with the same topic
+        producer.produce(target_topic_name, key=msg.key(), value=msg.value())
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    consumer.close()
+    producer.flush()
+
+print("Message replication completed.")
